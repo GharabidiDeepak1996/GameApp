2da7a5f7e377e6c291feb815217d4a190187bef3
Sarvesh Doshi <sdoshi@plexitech.in>
2023-03-21 16:46:25 +0530

    cardview

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { StyleSheet,View} from "react-native";
+import { Dimensions, StyleSheet,View} from "react-native";
 import Colors from "../../constants/colors";
 
 export default function Card({childen}){
     return <View style={styles.card}>{childen}</View>
 }
 
+const deviceWidth = Dimensions.get('window').width
+
 const styles = StyleSheet.create({
     card:{
         justifyContent:'center',
         alignItems:'center',
-        marginTop:36,
+        marginTop: deviceWidth <380 ? 18 : 36 ,
         marginHorizontal:24,
         padding:16,
         backgroundColor: Colors.primary800,
